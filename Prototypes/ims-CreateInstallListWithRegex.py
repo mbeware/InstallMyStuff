e953ae5dc662991b856c15ca0f4fb854e0772661
mbeware <github@mbeware.ca>
2024-11-07 23:04:50 -0500

    s

--- Prototypes/ims-CreateInstallListWithRegex.py
+++ Prototypes/ims-CreateInstallListWithRegex.py
@@ -10,11 +10,13 @@ with open(log_path, 'r') as file:
     log_block = file.read()
 
 
-
+####
+#  The date is wrong, because some block don't have "Install" or "Remove". We need to accept all blocks 
+#  and we will filter later for Install and Remove
 
 # Updated regex pattern to capture each block with Start-Date, Install/Remove, and package details
 pattern = re.compile(
-    r'Start-Date:\s+(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2})\n.*?\n(Install|Remove):\s+([^E]+?)\nEnd-Date:',
+    r'Start-Date:\s+(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2})\n.*?\n(Install|Update|Upgrade|Remove):\s+([^E]+?)\nEnd-Date:',
     re.MULTILINE | re.DOTALL
 )
 
@@ -25,12 +27,16 @@ for match in pattern.finditer(log_block):
     action = match.group(2)
     packages = match.group(3)
     
-    # Find individual package names without "automatic" in parentheses
-    package_list = re.findall(r'([\w\-.+]+):\w+\s+\((?!.*automatic).*?\)', packages)
-    
-    
-    # Append each package with date and action to the results
-    for package in package_list:
-        results.append((date, action, package))
-        print(f"{package},{date},{action}")
+    if action in ['Install', 'Remove']:
+        # Find individual package names without "automatic" in parentheses
+        # package_list = re.findall(r'([\w\-.+]+):\w+\s+\((?!.*automatic).*?\)', packages)
+        package_list = re.findall(r"([\w\-.+]+):\w+\s+\((.*?)\)", packages)
+        
+        
+        # Append each package with date and action to the results
+        for package,info in package_list:
+            if "automatic" in info:
+                continue
+            results.append((date, action, package))
+            print(f"{package},{date},{action}")
 
